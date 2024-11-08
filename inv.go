b6420929fbe3bc423785ba67ed7ed5ccbfdea1ec
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2024-11-08 18:43:36 +0100

    kinda work

--- inv.go
+++ inv.go
@@ -14,6 +14,9 @@ type vars map[string]string
 
 func (i inv) MarshalJSON() ([]byte, error) {
 	dirty_inv := make(map[string]interface{})
+	for name, group := range i.groups {
+		dirty_inv[name] = group
+	}
 	dirty_inv["_meta"] = i.meta
 
 	result, err := json.Marshal(dirty_inv)
